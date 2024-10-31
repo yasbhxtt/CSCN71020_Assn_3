2ba9ddd23edfbdbe642a2b08f67067ea5900d22e
Yash <yash.b@live.ca>
2024-10-30 22:45:25 -0400

    Added 3 more test functions(Failing tests)

--- Rock_Paper_Scissors/main.c
+++ Rock_Paper_Scissors/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
+
 const char* Game_OUTCOME(const char* player1, const char* player2) {
 	if (	//if player 1 chooses paper and player 2 chooses rock
 		(strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0)) {
@@ -20,4 +21,5 @@ const char* Game_OUTCOME(const char* player1, const char* player2) {
 		(strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0)) {
 		return "Player1";
 	}
+
 }
--- Unit_Test_RPS/Unit_Test_RPS.cpp
+++ Unit_Test_RPS/Unit_Test_RPS.cpp
@@ -18,6 +18,31 @@ namespace UnitTestRPS
 			Assert::AreEqual("Player1", Game_OUTCOME("Paper", "Rock"));
 			Assert::AreEqual("Player1", Game_OUTCOME("Rock", "Scissors"));
 		}
+		
+		TEST_METHOD(TestPlayer2Winning)
+		{
+			Assert::AreEqual("Player2", Game_OUTCOME("Paper", "Scissors"));
+			Assert::AreEqual("Player2", Game_OUTCOME("Scissors", "Rock"));
+			Assert::AreEqual("Player2", Game_OUTCOME("Rock", "Paper"));
+		}
+
+		TEST_METHOD(TestPlayersDraw)
+		{
+			Assert::AreEqual("Draw", Game_OUTCOME("Paper", "Paper"));
+			Assert::AreEqual("Draw", Game_OUTCOME("Scissors", "Scissors"));
+			Assert::AreEqual("Draw", Game_OUTCOME("Rock", "Rock"));
+		}
+
+		TEST_METHOD(TestInvalidInput)
+		{
+			Assert::AreEqual("INVALID INPUT!", Game_OUTCOME("@#$WF", "Paper"));
+			Assert::AreEqual("INVALID INPUT!", Game_OUTCOME("", "Scissors"));
+			Assert::AreEqual("INVALID INPUT!", Game_OUTCOME("1232", "Rock"));
+			Assert::AreEqual("INVALID INPUT!", Game_OUTCOME("OWL", "Paper"));
+			Assert::AreEqual("INVALID INPUT!", Game_OUTCOME("SCISSOR23", "Fo3rk"));
+			Assert::AreEqual("INVALID INPUT!", Game_OUTCOME("SPoon", "Rock"));
+		}
+
 
 	};
 }
