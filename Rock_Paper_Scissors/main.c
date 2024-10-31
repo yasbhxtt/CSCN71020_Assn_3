af49f1e0216dce1edbff7af40f6def93f483b87b
Yash <yash.b@live.ca>
2024-10-30 22:48:15 -0400

    Created main function to pass second test

--- Rock_Paper_Scissors/main.c
+++ Rock_Paper_Scissors/main.c
@@ -22,4 +22,22 @@ const char* Game_OUTCOME(const char* player1, const char* player2) {
 		return "Player1";
 	}
 
+
+	//FOR SECOND TEST
+	if (	//if player 2 chooses paper and player 1 chooses rock
+		(strcmp(player2, "Paper") == 0 && strcmp(player1, "Rock") == 0)) {
+		return "Player2";
+	}
+
+	if (	//if player 2 chooses rock and player 1 chooses scissors
+		(strcmp(player2, "Rock") == 0 && strcmp(player1, "Scissors") == 0)) {
+		return "Player2";
+	}
+
+	if (	//if player 2 chooses scissors and player 1 chooses paper
+		(strcmp(player2, "Scissors") == 0 && strcmp(player1, "Paper") == 0)) {
+		return "Player2";
+	}
+
+
 }
